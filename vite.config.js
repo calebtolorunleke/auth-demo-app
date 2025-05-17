1fc247a7d63c85d4e017987abde3b1459416221c
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-17 17:48:43 +0100

    demo app change

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),
   tailwindcss()],
-  base: '/auth-demo-app/',
-
-  esbuild: {
-    loader: 'jsx',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    }
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      }
-    }
-  }
-
 })
